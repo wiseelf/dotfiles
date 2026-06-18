483ed1bfd869a7c4a4fdc76e676b511434aecdbe
wiseelf <wiseelf@gmail.com>
2026-06-18 15:06:17 +1000

    fix: use terminfo for Up/Down bindings to work across terminal types

--- zsh/.zsh/keybindings.zsh
+++ zsh/.zsh/keybindings.zsh
@@ -2,6 +2,10 @@ autoload -Uz select-word-style
 select-word-style bash
 
 # History substring search (widgets provided by the plugin)
+zmodload zsh/terminfo
+bindkey "${terminfo[kcuu1]}" history-substring-search-up
+bindkey "${terminfo[kcud1]}" history-substring-search-down
+# ANSI fallback for terminals that don't populate terminfo
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 # Home / End
